a00e86b0c2e9dac5b466f1519aeeab474db871f9
James Lucktaylor <jlucktay+github@gmail.com>
2020-04-12 07:59:10 +0100

    Set up go-import vanity path metadata.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,10 @@
-import * as functions from 'firebase-functions';
+import { https } from 'firebase-functions';
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
+const go_vcs_base = "https://github.com/jlucktay"
+
+export const goPackage = https.onRequest((request, response) => {
+  response.send(`<meta name=go-import content="${request.hostname}${request.path} git ${go_vcs_base}${request.path}">`)
 });
