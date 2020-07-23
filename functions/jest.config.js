58b8176bcbeaa1b3af5332a0a9850567d00f5d08
James Lucktaylor <jlucktay+github@gmail.com>
2020-07-23 14:42:29 +0100

    test(jest): update config

--- functions/jest.config.js
+++ functions/jest.config.js
@@ -1,5 +1,9 @@
+// This config includes TypeScript-specific settings.
+// If you're not using TypeScript, you should remove the `transform` property.
 module.exports = {
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   preset: "ts-jest",
   testEnvironment: "node",
   testPathIgnorePatterns: ["<rootDir>/lib/", "<rootDir>/node_modules/"],
+  transform: { "^.+\\.tsx?$": "ts-jest" },
 };
