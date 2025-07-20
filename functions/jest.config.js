f913be584ef816c271d62d8ac2a88592ed83dc9b
James Lucktaylor <jlucktay+github@gmail.com>
2025-07-20 04:54:17 +0100

    chore(go.jlucktay.dev): cleanup
    No longer needed now that the Rust implementation is running on a Cloudflare Worker.

--- functions/jest.config.js
+++
@@ -1,9 +0,0 @@
-// This config includes TypeScript-specific settings.
-// If you're not using TypeScript, you should remove the `transform` property.
-module.exports = {
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  preset: "ts-jest",
-  testEnvironment: "node",
-  testPathIgnorePatterns: ["<rootDir>/lib/", "<rootDir>/node_modules/"],
-  transform: { "^.+\\.tsx?$": "ts-jest" },
-};
